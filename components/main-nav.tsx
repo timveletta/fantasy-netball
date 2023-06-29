188737b3be46cc7720a8c4b8a8137a80a72717c5
Tim Veletta <tim.veletta@mechanicalrock.io>
2023-06-29 23:38:51 +0800

    add user sign up and sign in with clerk

---
+++ components/main-nav.tsx
@@ -0,0 +1,46 @@
+import Link, { LinkProps } from 'next/link';
+
+import { cn } from '@/utils';
+import { UserButton, auth } from '@clerk/nextjs';
+
+const NavLink = ({
+	className,
+	...props
+}: LinkProps & React.HTMLAttributes<HTMLElement>) => {
+	return (
+		<Link
+			className={cn(
+				'text-sm font-medium transition-colors hover:text-primary',
+				className
+			)}
+			{...props}
+		></Link>
+	);
+};
+
+export function MainNav({
+	className,
+	...props
+}: React.HTMLAttributes<HTMLElement>) {
+	const { userId } = auth();
+
+	const isLoggedIn = !!userId;
+
+	return (
+		<nav className={cn('py-4 border-b border-gray-300', className)} {...props}>
+			<div className="flex  justify-between container">
+				<div>Fantasy Netball</div>
+				<div className="flex space-x-4 lg:space-x-6">
+					{isLoggedIn && <NavLink href="/my-teams">My Teams</NavLink>}
+					{!isLoggedIn && (
+						<>
+							<NavLink href="/sign-in">Login</NavLink>
+							<NavLink href="/sign-up">Register</NavLink>
+						</>
+					)}
+					<UserButton afterSignOutUrl="/" />
+				</div>
+			</div>
+		</nav>
+	);
+}
