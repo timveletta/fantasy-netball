8a54cb2857b476776396aeb7a128c50de1d6af95
Tim Veletta <tim.veletta@mechanicalrock.io>
2023-06-30 01:20:51 +0800

    trying module.exports

--- api/user-created-event.ts
+++ api/user-created-event.ts
@@ -1,10 +1,6 @@
-import type { WebhookEvent } from '@clerk/clerk-sdk-node';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function handler(
-	request: VercelRequest,
-	response: VercelResponse
-) {
+module.exports = (request: VercelRequest, response: VercelResponse) => {
 	console.log(request);
 
 	response.status(200).json({
@@ -12,4 +8,4 @@ export default function handler(
 		query: request.query,
 		cookies: request.cookies,
 	});
-}
+};
