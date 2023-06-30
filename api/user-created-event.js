33933bd2b9028dd3165373eb160d84cde2e1f8fc
Tim Veletta <tim.veletta@mechanicalrock.io>
2023-06-30 12:36:37 +0800

    add user-created api endpoint

--- api/user-created-event.js
+++
@@ -1,9 +0,0 @@
-module.exports = (request, response) => {
-	console.log(request.body);
-
-	response.status(200).json({
-		body: request.body,
-		query: request.query,
-		cookies: request.cookies,
-	});
-};
