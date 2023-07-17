ee1316e4e49d25254003b7505f3047cb9caa22d1
Tim Veletta <timothy.veletta@gmail.com>
2023-07-17 14:48:34 +0800

    make add button look distinct

--- components/team-tile.tsx
+++ components/team-tile.tsx
@@ -1,24 +1,26 @@
-import { cn } from '@/utils';
-import Link from 'next/link';
-import React from 'react';
+import { cn } from "@/utils";
+import Link from "next/link";
+import React from "react";
 
 type TeamTileProps = {
-	id?: string;
-	name?: string | null;
-	isEmpty?: boolean;
+  id?: string;
+  name?: string | null;
+  isEmpty?: boolean;
 };
 
 const TeamTile = ({ id, name, isEmpty = false }: TeamTileProps) => {
-	return (
-		<Link
-			className={cn(
-				'w-64 h-64 mr-4 mb-4 flex justify-center items-center bg-slate-100 rounded-lg cursor-pointer hover:bg-slate-200'
-			)}
-			href={isEmpty ? '/my-teams/create' : `/my-teams/${id}`}
-		>
-			{isEmpty ? <>Add Team</> : <>{name}</>}
-		</Link>
-	);
+  return (
+    <Link
+      className={cn(
+        "w-64 h-64 mr-4 mb-4 flex justify-center items-center rounded-lg cursor-pointer ",
+        isEmpty && "border-4 border-dashed border-slate-400 hover:bg-slate-200",
+        !isEmpty && "bg-slate-100 hover:bg-slate-200"
+      )}
+      href={isEmpty ? "/my-teams/create" : `/my-teams/${id}`}
+    >
+      {isEmpty ? <>Add Team</> : <>{name}</>}
+    </Link>
+  );
 };
 
 export default TeamTile;
