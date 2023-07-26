677ed86c0a672ff47a4fdc66a61fb22c4bb3a03e
Tim Veletta <timothy.veletta@gmail.com>
2023-07-26 12:03:37 +0800

    adding container to hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 const Hero = () => {
   return (
     <div className="relative">
-      <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-12 lg:px-8">
+      <div className="container lg:grid lg:grid-cols-12 lg:gap-x-12 lg:px-8">
         <div className="px-6 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-48 xl:col-span-6">
           <div className="mx-auto max-w-2xl lg:mx-0">
             <Text.Title>
