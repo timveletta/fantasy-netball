40efbafc2e980e50218787f7c04d52173cb94c3a
Tim Veletta <timothy.veletta@gmail.com>
2023-08-08 11:27:07 +0800

    a user can search for players

--- components/player-list.tsx
+++ components/player-list.tsx
@@ -1,9 +1,17 @@
 "use client";
 import { Position, Prisma } from "@prisma/client";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import * as z from "zod";
 import React from "react";
 import PlayerListTile from "./player-list-tile";
 import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
 import { Input } from "./ui/input";
+
+const formSchema = z.object({
+  searchTerm: z.string(),
+});
 
 type PlayerListTileProps = {
   players: Prisma.PlayerGetPayload<{ include: { team: true } }>[];
@@ -12,6 +20,11 @@ type PlayerListTileProps = {
 };
 
 const PlayerList = ({ players, onAddPlayer, isTeamFull }: PlayerListTileProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { searchTerm: "" },
+  });
+  const watchSearchTerm = form.watch("searchTerm");
   const [selectedPosition, setSelectedPosition] = React.useState<"ALL" | Position>("ALL");
 
   const onTabChange = (value: string) => {
@@ -21,28 +34,47 @@ const PlayerList = ({ players, onAddPlayer, isTeamFull }: PlayerListTileProps) =
   };
 
   const filteredPlayers = React.useMemo(() => {
-    if (selectedPosition === "ALL") {
-      return players;
-    }
-
-    return players.filter((player) => player.position === selectedPosition);
-  }, [players, selectedPosition]);
+    return players
+      .filter((player) => selectedPosition === "ALL" || player.position === selectedPosition)
+      .filter((player) =>
+        `${player.firstName} ${player.lastName} ${player.team.name}`
+          .toLowerCase()
+          .includes(watchSearchTerm.toLowerCase())
+      );
+  }, [players, selectedPosition, watchSearchTerm]);
 
   return (
-    <div className="">
-      <Input placeholder="Search" />
-      <Tabs value={selectedPosition} onValueChange={onTabChange}>
-        <TabsList>
-          <TabsTrigger value="ALL">ALL</TabsTrigger>
-          <TabsTrigger value={Position.GK}>GK</TabsTrigger>
-          <TabsTrigger value={Position.GD}>GD</TabsTrigger>
-          <TabsTrigger value={Position.WD}>WD</TabsTrigger>
-          <TabsTrigger value={Position.C}>C</TabsTrigger>
-          <TabsTrigger value={Position.WA}>WA</TabsTrigger>
-          <TabsTrigger value={Position.GA}>GA</TabsTrigger>
-          <TabsTrigger value={Position.GS}>GS</TabsTrigger>
-        </TabsList>
-      </Tabs>
+    <div className="flex flex-col gap-y-4">
+      <Form {...form}>
+        <form autoComplete="off">
+          <FormField
+            control={form.control}
+            name="searchTerm"
+            render={({ field, fieldState }) => (
+              <FormItem>
+                <FormControl>
+                  <Input {...field} placeholder="Search players..." />
+                </FormControl>
+                {fieldState.error && <FormMessage>{fieldState.error.message}</FormMessage>}
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+      <div>
+        <Tabs value={selectedPosition} onValueChange={onTabChange}>
+          <TabsList>
+            <TabsTrigger value="ALL">ALL</TabsTrigger>
+            <TabsTrigger value={Position.GK}>GK</TabsTrigger>
+            <TabsTrigger value={Position.GD}>GD</TabsTrigger>
+            <TabsTrigger value={Position.WD}>WD</TabsTrigger>
+            <TabsTrigger value={Position.C}>C</TabsTrigger>
+            <TabsTrigger value={Position.WA}>WA</TabsTrigger>
+            <TabsTrigger value={Position.GA}>GA</TabsTrigger>
+            <TabsTrigger value={Position.GS}>GS</TabsTrigger>
+          </TabsList>
+        </Tabs>
+      </div>
       <ul className="flex flex-col gap-y-2">
         {filteredPlayers.map((player) => (
           <PlayerListTile key={player.id} onClick={onAddPlayer} disabled={isTeamFull} {...player} />
