7440c2196b0d8b51ceda6116444ec6c897290760
Tim Veletta <timothy.veletta@gmail.com>
2023-09-11 15:40:46 +0800

    Revert "remove images from middleware requests"
    This reverts commit a078a36c3ebb06bbb3911dbf4ec05f3123cc4bf2.

--- middleware.ts
+++ middleware.ts
@@ -31,6 +31,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    "/((?!api|_next/static|_next/image|favicon.ico|heroimage.jpg|logo.png).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
   ],
 };
